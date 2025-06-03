0a288156b204256071f7364fd670c2366c34e9fd
hyukjin <hyukjin.kim.dev@gmail.com>
2025-06-03 19:48:36 +0900

    변수

---
+++ java-start/java-start/src/varible/Var2.java
@@ -0,0 +1,12 @@
+package varible;
+
+public class Var2 {
+
+    public static void main(String[] args){
+        int a;
+        a = 10;
+        System.out.println(a);
+        System.out.println(a);
+        System.out.println(a);
+    }
+}
